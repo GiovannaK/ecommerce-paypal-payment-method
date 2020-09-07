d96aa0ca880860fd4afc251630999f1d016dde07
Giovanna Cunha <giovannakelli@gmail.com>
2020-09-06 23:11:12 -0300

    change debug to false

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ecom-store-electronics.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
